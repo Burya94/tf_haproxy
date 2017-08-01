078f4601b06e935dea2df07f6bc61f1a175dc8bc
Artem Buria <artem_buria@epam.com>
2017-08-01 17:20:47 +0300

    provision changes

--- logstash.sh
+++ logstash.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-yum update -y
-yum install wget -y
-cd /opt && wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
-cd /opt && tar xzvf jdk-8u141-linux-x64.tar.gz
-cat >> /root/.bash_profile << EOF
-export JAVA_HOME=/opt/jdk1.8.0_141/
-export PATH=\${PATH}:\${JAVA_HOME}bin/
-EOF
+yum install java -y
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 cat >> /etc/yum.repos.d/logstash.repo << EOF
 [logstash-2.2]
