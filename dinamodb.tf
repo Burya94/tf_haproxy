71d35c344eccd5cc4db7ee9dc4145298159a390d
Artem Buria <artem_buria@epam.com>
2017-08-16 14:49:10 +0300

    dynamodb upd

--- dinamodb.tf
+++ dinamodb.tf
@@ -1,12 +1,12 @@
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "${var.table_name}"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "leaseKey"
 
 
   attribute {
-    name = "UserId"
+    name = "leaseKey"
     type = "S"
   }
   }
